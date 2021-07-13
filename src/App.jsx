945b7d98bbe37a3aa4c5d233fb38d5a6e7ad8e62
yoritin <47884591+yoritin@users.noreply.github.com>
2021-07-13 15:55:38 +0900

    リファクタリング

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,21 @@
 import React, { useState } from 'react'
 
 const App = (props) => {
-  const [name, setName] = useState(props.name)
-  const [price, setPrice] = useState(props.price)
-
-  const reset = () => {
-    setPrice(props.price)
-    setName(props.name)
-  }
+  const [state, setState] = useState(props)
 
   return (
     <>
       <p>
-        現在の{name}は、{price}円です。
+        現在の{state.name}は、{state.price}円です。
       </p>
-      <button onClick={() => setPrice(price + 1)}>+1</button>
-      <input value={name} onChange={(e) => setName(e.target.value)} />
-      <button onClick={reset}>Reset</button>
+      <button onClick={() => setState({ ...state, price: state.price + 1 })}>
+        +1
+      </button>
+      <input
+        value={state.name}
+        onChange={(e) => setState({ ...state, name: e.target.value })}
+      />
+      <button onClick={() => setState(props)}>Reset</button>
     </>
   )
 }
