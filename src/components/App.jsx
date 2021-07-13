9bcee23b82c2ff5787de3cd8b321ce1d886d238d
yoritin <47884591+yoritin@users.noreply.github.com>
2021-07-13 17:25:58 +0900

    bootstrapを導入してformを実装する

---
+++ src/components/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const App = () => {
+  return (
+    <>
+      <div className="container-fluid">
+        <h4>イベント作成フォーム</h4>
+        <form>
+          <div className="form-group">
+            <label htmlFor="formEventTitle">タイトル</label>
+            <input className="form-control" id="formEventTitle" />
+          </div>
+        </form>
+      </div>
+    </>
+  )
+}
+
+App.defaultProps = {
+  name: '',
+  price: 1000,
+}
+
+export default App
